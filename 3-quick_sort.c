b04c5e93faacc22186e91983d2c230a68303f63c
ouailb <adomeyou@gmail.com>
2023-10-23 14:02:53 +0100

    t+3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,66 +1,64 @@
 #include "sort.h"
 
 /**
- * swap_items - Swaps two items in an array.
+ * Items_swap - Swaps two .
  * @array: The array to modify.
  * @l: The index of the left item.
  * @r: The index of the right item.
  */
-void swap_items(int *array, size_t l, size_t r)
+void Items_swap(int *array, size_t l, size_t r)
 {
-	int tmp;
+	int index;
 
 	if (array != NULL)
 	{
-		tmp = array[l];
+		index = array[l];
 		array[l] = array[r];
-		array[r] = tmp;
+		array[r] = index;
 	}
 }
 
 /**
- * quick_sort_range_lomuto - Sorts a sub array using the
- * quick sort algorithm and Lomuto's partition scheme.
+ * implement the Lomuto - Sorts a sub array
  * @array: The array containing the sub-array.
  * @low: The starting position of the sub-array.
  * @high: The ending position of the sub-array.
  * @size: The length of the array.
  */
-void quick_sort_range_lomuto(int *array, size_t low, size_t high, size_t size)
+void implement_Lomuto(int *array, size_t low, size_t high, size_t size)
 {
-	size_t k, i;
+	size_t x, i;
 	int pivot;
 
 	if ((low >= high) || (array == NULL))
 		return;
 	pivot = array[high];
-	k = low;
+	x = low;
 	for (i = low; i < high; i++)
 	{
 		if (array[i] <= pivot)
 		{
-			if (k != i)
+			if (x != i)
 			{
-				swap_items(array, k, i);
+				Items_swap(array, x, i);
 				print_array(array, size);
 			}
-			k++;
+			x++;
 		}
 	}
-	if (k != high)
+	if (x != high)
 	{
-		swap_items(array, k, high);
+		Items_swap(array, x, high);
 		print_array(array, size);
 	}
-	if (k - low > 1)
-		quick_sort_range_lomuto(array, low, k - 1, size);
-	if (high - k > 1)
-		quick_sort_range_lomuto(array, k + 1, high, size);
+	if (x - low > 1)
+		implement_Lomuto(array, low, x - 1, size);
+	if (high - x > 1)
+		implement_Lomuto(array, x + 1, high, size);
 }
 
 /**
- * quick_sort - Sorts an array using the quick sort algorithm
- * and Lomuto's partition scheme.
+ * quick_sort - Sorts an array
  * @array: The array to sort.
  * @size: The length of the array.
  */
@@ -68,6 +66,6 @@ void quick_sort(int *array, size_t size)
 {
 	if (array != NULL)
 	{
-		quick_sort_range_lomuto(array, 0, size - 1, size);
+		implement_Lomuto(array, 0, size - 1, size);
 	}
 }
